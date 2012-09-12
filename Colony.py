42a0eff45cac94f4ddae62168d24bd979cab64f0
gelatindesign <info@gelatindesign.co.uk>
2012-09-13 00:21:32 +0100

    Initial Commit

---
+++ Colony.py
@@ -0,0 +1,15 @@
+# -------- Colony --------
+# @author Gelatin Design, Laurence Roberts
+# @datestart 12th September 2012
+# 
+# @version Pre-Alpha 0.0
+# -----------------------------
+
+# Load config
+
+# Import app logic
+from app.App import App
+
+# Create app
+app = App( )
+app.run( )
---
+++ app/App.py
@@ -0,0 +1,16 @@
+# -------- App.py --------
+# Handles all general application logic
+# ------------------------
+
+# Imports
+from direct.showbase.ShowBase import ShowBase
+
+# -------- App --------
+class App( ShowBase ):
+
+	# Init
+	# 
+	# @param object self
+	# @return void
+	def __init__( self ):
+		ShowBase.__init__( self )
